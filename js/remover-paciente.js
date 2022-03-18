ac3fdfd012d44196a8d53a688cba4b6076d2b412
Matheus <matheusvsoliveira61@gmail.com>
2022-03-18 12:16:28 -0300

    Adicionando animação ao remover paciente da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,5 +1,13 @@
 var tabela = document.querySelector("#tabela-pacientes")
 
 tabela.addEventListener("dblclick", function(event){
-    event.target.parentNode.remove(); // Remove o pai do elemento na árvore DOM
+    
+    event.target.parentNode.classList.add("fadeOut");
+   
+    //função que adiciona um intervalo até que outra função seja executada
+    setTimeout(function(){ 
+        event.target.parentNode.remove(); // Função que remove o pai no nó HTML
+    },400)
+   
+     
 });
