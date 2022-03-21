8447d8418d5912a384a58369d2cf8ee9115efd93
Matheus <matheusvsoliveira61@gmail.com>
2022-03-21 12:32:24 -0300

    Adicionando a classe ContaCorrente e criando métodos

--- Javascript/orientacao-objetos/index.js
+++ Javascript/orientacao-objetos/index.js
@@ -1,21 +1,42 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
+   
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente {
+    agencia;
+    saldo;
 
+    sacar(valor) {
+        if(this.saldo >= valor)
+        {
+            this.saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor <= 0) 
+        {
+            return;
+        }
+        this.saldo += valor;
+    }
+}
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001
+
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
+
+console.log(contaCorrenteRicardo.saldo);
