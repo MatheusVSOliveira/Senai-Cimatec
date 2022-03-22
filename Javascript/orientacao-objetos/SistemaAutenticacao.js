6fc0ce6f59222784fe5a5f99c66af676f539dd57
Matheus <matheusvsoliveira61@gmail.com>
2022-03-22 11:23:26 -0300

    Adicionando cliente no sistema de autenticação

--- Javascript/orientacao-objetos/SistemaAutenticacao.js
+++ Javascript/orientacao-objetos/SistemaAutenticacao.js
@@ -1,5 +1,13 @@
-export class SistemaAutenticacao{
-    static login(funcionario, senha){
-       return funcionario.senha == senha;
+export class SistemaAutenticacao {
+    static login(usuario, senha) {
+        if (SistemaAutenticacao.autenticavel(usuario)) {
+            return usuario.autenticar(senha);
+        }
+        return false;
+    }
+
+    static autenticavel(usuario){
+      return  "autenticar" in usuario && 
+      usuario.autenticar instanceof Function
     }
 }
--- Javascript/orientacao-objetos/index.js
+++ Javascript/orientacao-objetos/index.js
@@ -24,15 +24,17 @@ console.log(contaCorrente);
 console.log(contaPoupanca);
 console.log(contaSalario);*/
 
-const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
-diretor.cadastrarSenha("123456789")
+const cliente = new Cliente("Lais", 78945612379, "456")
 
 const gerente =  new Gerente("Ricardo",  5000, 12378945601);
 gerente.cadastrarSenha("123")
 
-const estaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456789")
 
 
-console.log(estaLogado);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789");
 
-
+console.log(clienteEstaLogado,gerenteEstaLogado, diretorEstaLogado);
