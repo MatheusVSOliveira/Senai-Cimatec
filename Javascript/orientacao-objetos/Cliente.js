d89115b861d4b3a3136f247249f0499d4eb66424
Matheus <matheusvsoliveira61@gmail.com>
2022-03-22 08:28:50 -0300

    Adicionando a classe ContaPoupanca

--- Javascript/orientacao-objetos/Cliente.js
+++ Javascript/orientacao-objetos/Cliente.js
@@ -1,7 +1,4 @@
 export class Cliente {
-    nome;
-    _cpf;
-   
     get cpf(){
         this._cpf;
     }
@@ -9,5 +6,26 @@ export class Cliente {
     constructor(nome, cpf){
         this.nome = nome;
         this._cpf = cpf;
+    }
+
+    sacar(valor) {
+        if(this._saldo >= valor)
+        {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor <= 0) 
+        {
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    transferir(valor,conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
